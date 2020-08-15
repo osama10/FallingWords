9097eb79a06cd6fd391409a3b72544e00ca1577c
obashir <osama.bashir@zalando.de>
2020-08-15 20:25:26 +0200

    add GameData and Word Entities

---
+++ FallingWords/FallingWords/Domain/GamePlay/Entity/GameData.swift
@@ -0,0 +1,14 @@
+//
+//  GameData.swift
+//  FallingWords
+//
+//  Created by Osama Bashir on 8/15/20.
+//  Copyright © 2020 Osama Bashir. All rights reserved.
+//
+
+import Foundation
+
+struct GameData {
+    let word: String
+    let options: [String]
+}
---
+++ FallingWords/FallingWords/Domain/GamePlay/Entity/Word.swift
@@ -0,0 +1,15 @@
+//
+//  Word.swift
+//  FallingWords
+//
+//  Created by Osama Bashir on 8/15/20.
+//  Copyright © 2020 Osama Bashir. All rights reserved.
+//
+
+import Foundation
+
+struct Word {
+    let word: String
+    let translation: String
+}
+
