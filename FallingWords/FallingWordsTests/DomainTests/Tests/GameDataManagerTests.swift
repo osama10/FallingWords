6154b7966790ca052c72814d8c3d0e1ba2d85f02
obashir <osama.bashir@zalando.de>
2020-08-17 17:15:19 +0200

    added tests for GameDataManager class

---
+++ FallingWords/FallingWordsTests/DomainTests/Tests/GameDataManagerTests.swift
@@ -0,0 +1,58 @@
+//
+//  GameDataManagerTests.swift
+//  FallingWordsTests
+//
+//  Created by Osama Bashir on 8/17/20.
+//  Copyright © 2020 Osama Bashir. All rights reserved.
+//
+
+import XCTest
+@testable import FallingWords
+
+class GameDataManagerTests: XCTestCase {
+
+    let manager = GameDataManager()
+    
+    func test_whenEmptyArrayPassToPrepareData_ItProduceCorrectResults() {
+        manager.prepareGameData(words: [])
+        XCTAssertEqual(manager.data.count, 0)
+        XCTAssertEqual(manager.dataDict.count, 0)
+        XCTAssertEqual(manager.originalWords.count, 0)
+        XCTAssertEqual(manager.translations.count, 0)
+
+    }
+
+    func test_whenArrayWithLessThanFourElementsIsPassToPrepareData_ItProduceCorrectResults() {
+        let words = [Word(word: "1", translation: "One"), Word(word: "2", translation: "Two"), Word(word: "3", translation: "Three")]
+        manager.prepareGameData(words: words)
+        XCTAssertEqual(manager.data.count, 3)
+        XCTAssertEqual(manager.data[0].options.count, 3)
+        XCTAssertEqual(manager.dataDict.count, 3)
+        XCTAssertEqual(manager.originalWords.count, 3)
+        XCTAssertEqual(manager.translations.count, 3)
+
+    }
+
+    func test_whenArrayWithFourElementsIsPassToPrepareData_ItProduceCorrectResults() {
+        let words = [Word(word: "1", translation: "One"), Word(word: "2", translation: "Two"), Word(word: "3", translation: "Three"), Word(word: "4", translation: "Four")]
+        manager.prepareGameData(words: words)
+        XCTAssertEqual(manager.data.count, 4)
+        XCTAssertEqual(manager.data[0].options.count, 4)
+        XCTAssertEqual(manager.dataDict.count, 4)
+        XCTAssertEqual(manager.originalWords.count, 4)
+        XCTAssertEqual(manager.translations.count, 4)
+
+    }
+
+    func test_whenArrayWithMoreThanFourElementsIsPassToPrepareData_ItProduceCorrectResults() {
+         let words = [Word(word: "1", translation: "One"), Word(word: "2", translation: "Two"), Word(word: "3", translation: "Three"), Word(word: "4", translation: "Four"), Word(word: "5", translation: "Five")]
+         manager.prepareGameData(words: words)
+         XCTAssertEqual(manager.data.count, 5)
+         XCTAssertEqual(manager.data[0].options.count, 4)
+         XCTAssertEqual(manager.dataDict.count, 5)
+         XCTAssertEqual(manager.originalWords.count, 5)
+         XCTAssertEqual(manager.translations.count, 5)
+
+     }
+
+}
