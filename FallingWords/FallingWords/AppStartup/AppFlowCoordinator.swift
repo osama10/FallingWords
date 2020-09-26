8acdb23fe1c40e4270b46f6d32bb7590dde57b21
obashir <osama.bashir@zalando.de>
2020-09-26 23:35:35 +0200

    adds AppFlowCoordinator remove AppNavigator Create AppDIContainer Create GameSceneDIContainer

---
+++ FallingWords/FallingWords/AppStartup/AppFlowCoordinator.swift
@@ -0,0 +1,9 @@
+//
+//  AppFlowCoordinator.swift
+//  FallingWords
+//
+//  Created by Osama Bashir on 9/26/20.
+//  Copyright © 2020 Osama Bashir. All rights reserved.
+//
+
+import Foundation
---
+++ FallingWords/FallingWords/AppStartup/DIContainer/GameSceneDIContainer.swift
@@ -0,0 +1,9 @@
+//
+//  GameSceneDIContainer.swift
+//  FallingWords
+//
+//  Created by Osama Bashir on 9/26/20.
+//  Copyright © 2020 Osama Bashir. All rights reserved.
+//
+
+import Foundation
